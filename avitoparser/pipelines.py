0ccc685cf18b5bf7710b75fb40910eaf8ff96d7f
HPC <mckena@mail.ru>
2022-02-14 21:05:13 +0300

    l7

--- avitoparser/pipelines.py
+++ avitoparser/pipelines.py
@@ -5,10 +5,15 @@
 
 
 # useful for handling different item types with a single interface
+import hashlib
+import mimetypes
+import os
+
 import scrapy
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
 from scrapy.pipelines.images import ImagesPipeline
+from scrapy.utils.python import to_bytes
 
 
 class AvitoparserPipeline:
@@ -59,3 +64,15 @@ class LeroymerlinImagePipeline(ImagesPipeline):
         item['photos'] = [itm[1] for itm in results if itm[0]]
         print(item['photos'])
         return item
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        media_guid = hashlib.sha1(to_bytes(request.url)).hexdigest()
+        media_ext = os.path.splitext(request.url)[1]
+        # Handles empty and wild extensions by trying to guess the
+        # mime type then extension or default to empty string otherwise
+        if media_ext not in mimetypes.types_map:
+            media_ext = ''
+            media_type = mimetypes.guess_type(request.url)[0]
+            if media_type:
+                media_ext = mimetypes.guess_extension(media_type)
+        return f'{item["_id"]}/{item["_id"]}-{media_guid}{media_ext}'
